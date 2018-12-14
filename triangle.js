ccacc39642a5c04bc8f757fb26bd55e8689ea225
Berthedusabeyezu <berthe1030@gmail.com>
2018-12-14 14:19:54 +0200

    Title

---
+++ triangle.js
@@ -0,0 +1,5 @@
+function triangleTracker(){
+
+    var x = parseInt(document.getElementById("x").value);
+    var y = parseInt(document.getElementById("y").value);
+    var z = parseInt(document.getElementById("z").value);
