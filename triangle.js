c0b107e586a73b257bfb4b05939e15f64c7a07ca
Berthedusabeyezu <berthe1030@gmail.com>
2018-12-14 14:49:55 +0200

    function of variables

--- triangle.js
+++ triangle.js
@@ -3,3 +3,4 @@ function triangleTracker(){
     var x = parseInt(document.getElementById("x").value);
     var y = parseInt(document.getElementById("y").value);
     var z = parseInt(document.getElementById("z").value);
+}
