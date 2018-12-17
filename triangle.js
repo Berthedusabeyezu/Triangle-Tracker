4db8c5fec61986554ca784da2584b91d4f8281f9
Berthedusabeyezu <berthe1030@gmail.com>
2018-12-17 14:08:09 +0200

    styling css

--- triangle.js
+++ triangle.js
@@ -3,17 +3,18 @@ function triangleTracker(){
     var x = parseInt(document.getElementById("x").value);
     var y = parseInt(document.getElementById("y").value);
     var z = parseInt(document.getElementById("z").value);
-    if (x ===y && y === z && z === x) {
+    if (x ===y && y === z ) {
         alert("Equilateral Triangle");
     }
+   
     else if (x === y || x === z || y === z ) {
         alert("Isosceles Triangle");
-    } 
-    
-    else if (x !== y && y !== z ) {
+    }
+      else if ((x+y)<=z || (x+z)<=y || (y+z)<=x) {
+        alert("THIS IS NOT A Triangle");
+    }
+      else if (x + y >z || y + z >x || x + z >y ) {
         alert("Scalene Triangle");
     }
-    else if ((x+y)<=z || (x+z)<=y || (y+z)<=x) {
-        alert("This is not a Triangle");
-    }
+   
 }
